e3fb72d13dbc7baa1a559e8369c0ae69bc6b5b54
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-01-31 23:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,36 @@ function convertToRoman(num) {
 
   //your code here
 
+	function romanConverter(num) {
+  let values = [
+    1000, 900, 500, 400,
+    100, 90, 50, 40,
+    10, 9, 5, 4, 1
+  ];
+
+  let romans = [
+    "M", "CM", "D", "CD",
+    "C", "XC", "L", "XL",
+    "X", "IX", "V", "IV", "I"
+  ];
+
+  let result = "";
+
+  for (let i = 0; i < values.length; i++) {
+    while (num >= values[i]) {
+      result += romans[i];
+      num -= values[i];
+    }
+  }
+
+  return result;
+}
+
+// Example
+console.log(romanConverter(14));   // XIV
+console.log(romanConverter(798));  // DCCXCVIII
+
+
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
